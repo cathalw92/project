fc5fb8a0635d56f07ea8edcfc36d3fe67fadee19
catha <cathalw92@gmail.com>
2021-05-13 00:23:29 +0100

    copy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import {Device1Component} from './modules/device1/device1.component';
+import {Device2Component} from './modules/device2/device2.component';
+import {Device3Component} from './modules/device3/device3.component';
+import {Device4Component} from './modules/device4/device4.component';
+import {Device5Component} from './modules/device5/device5.component';
+import {NotificationsComponent} from './modules/notifications/notifications.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
+import {SupportComponent} from './modules/support/support.component';
 
-const routes: Routes = [];
+// Creating routes to components that can be linked to in html files
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{path: '', component: DashboardComponent},
+    {path: 'device1', component: Device1Component},
+    {path: 'device2', component: Device2Component},
+    {path: 'device3', component: Device3Component},
+    {path: 'device4', component: Device4Component},
+    {path: 'device5', component: Device5Component},
+    {path: 'notifications', component: NotificationsComponent},
+    {path: 'support', component: SupportComponent}
+]
+}, {path: '**',  component: PagenotfoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
+import { Device2Component } from './modules/device2/device2.component';
+import { Device3Component } from './modules/device3/device3.component';
+import { Device4Component } from './modules/device4/device4.component';
+import { Device5Component } from './modules/device5/device5.component';
+import {FormsModule} from '@angular/forms';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import {MatTableModule} from '@angular/material/table';
+import { NotificationsComponent } from './modules/notifications/notifications.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { SupportComponent } from './modules/support/support.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+
+const config: SocketIoConfig = {
+  url: 'http://localhost:3000',
+  options: {}
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Device2Component,
+    Device3Component,
+    Device4Component,
+    Device5Component,
+    NotificationsComponent,
+    SupportComponent,
+    PagenotfoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        DefaultModule,
+        FormsModule,
+        SocketIoModule.forRoot(config),
+        MatTableModule,
+        MatCardModule,
+        MatExpansionModule,
+        MatListModule,
+        FlexLayoutModule,
+        MatIconModule,
+        MatTooltipModule,
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
